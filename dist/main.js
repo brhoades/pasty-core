95059d2e7c0985dac4197b035346ec39dcc848c7
Billy Rhoades <billyarhoades@gmail.com>
2017-08-03 18:01:39 -0500

    Switched to umd from commonjs... 0.0.4

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var crypto_1 = require("./crypto");
-exports.decryptFile = crypto_1.decryptFile;
-exports.encryptFile = crypto_1.encryptFile;
-var util_1 = require("./util");
-exports.randomPassword = util_1.randomPassword;
-exports.populateDefaults = util_1.populateDefaults;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "./crypto", "./util"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var crypto_1 = require("./crypto");
+    exports.decryptFile = crypto_1.decryptFile;
+    exports.encryptFile = crypto_1.encryptFile;
+    var util_1 = require("./util");
+    exports.randomPassword = util_1.randomPassword;
+    exports.populateDefaults = util_1.populateDefaults;
+});
