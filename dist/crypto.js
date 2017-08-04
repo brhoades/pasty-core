b4baef54bd70cbf9891ce34499469c915ecd065f
Billy Rhoades <billyarhoades@gmail.com>
2017-08-04 17:50:47 -0500

    Back to commonjs again.

--- dist/crypto.js
+++ dist/crypto.js
@@ -1,32 +1,22 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./util", "crypto-js/aes", "crypto-js/enc-utf8"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var util_1 = require("./util");
-    var CAES = require("crypto-js/aes");
-    var CENC = require("crypto-js/enc-utf8");
-    function decryptFile(data, key) {
-        var rawWords = CAES.decrypt(data, key);
-        return CENC.stringify(rawWords);
-    }
-    exports.decryptFile = decryptFile;
-    function encryptFile(b64data, keysize) {
-        if (keysize === void 0) { keysize = 32; }
-        var result = CENC.parse(b64data);
-        var password = util_1.randomPassword(keysize);
-        var encrypted = CAES.encrypt(result, password);
-        result = encrypted.toString();
-        return {
-            data: result,
-            key: password
-        };
-    }
-    exports.encryptFile = encryptFile;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = require("./util");
+var CAES = require("crypto-js/aes");
+var CENC = require("crypto-js/enc-utf8");
+function decryptFile(data, key) {
+    var rawWords = CAES.decrypt(data, key);
+    return CENC.stringify(rawWords);
+}
+exports.decryptFile = decryptFile;
+function encryptFile(b64data, keysize) {
+    if (keysize === void 0) { keysize = 32; }
+    var result = CENC.parse(b64data);
+    var password = util_1.randomPassword(keysize);
+    var encrypted = CAES.encrypt(result, password);
+    result = encrypted.toString();
+    return {
+        data: result,
+        key: password
+    };
+}
+exports.encryptFile = encryptFile;
--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,10 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./crypto", "./util", "./pastes/codefile"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var crypto_1 = require("./crypto");
-    exports.decryptFile = crypto_1.decryptFile;
-    exports.encryptFile = crypto_1.encryptFile;
-    var util_1 = require("./util");
-    exports.randomPassword = util_1.randomPassword;
-    exports.populateDefaults = util_1.populateDefaults;
-    var codefile_1 = require("./pastes/codefile");
-    exports.CodeFile = codefile_1.default;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var crypto_1 = require("./crypto");
+exports.decryptFile = crypto_1.decryptFile;
+exports.encryptFile = crypto_1.encryptFile;
+var util_1 = require("./util");
+exports.randomPassword = util_1.randomPassword;
+exports.populateDefaults = util_1.populateDefaults;
+var codefile_1 = require("./pastes/codefile");
+exports.CodeFile = codefile_1.default;
